89648caee0e745ae7ab82a81e2ff085f2e27182f
izamarro <ivanzamarro@gmail.com>
2014-05-16 16:29:33 +0200

    added delete job directory method

--- Test/test.py
+++ Test/test.py
@@ -17,16 +17,20 @@ class TestJobsDirectoryHandler(TestCase):
     def setUp(self):
 
         self.date_for_job = Mock()
-        self.date_for_job.directory_job = "/home/izamarro/workdev/burner2/Test/temp"
+        self.date_for_job.directory_job = "/home/izamarro/workdev/burner2/Test/temp/jobstest"
         self.date_for_job.patient_id = "9991"
         self.date_for_job.absolute_jobpath = self.date_for_job.directory_job + "/%s" % self.date_for_job.patient_id
 
         self.directory_creator = JobsDirectoryHandler(self.date_for_job.directory_job, self.date_for_job.patient_id, os)
         self.directory_creator.create_absolute_job_path_directory()
 
+        with open("/home/izamarro/workdev/burner2/Test/temp/orderstest/9991.DON", "w") as tempfile:
+            tempfile.close()
+
     def tearDown(self):
 
-        os.rmdir(self.date_for_job.absolute_jobpath)
+        os.remove("/home/izamarro/workdev/burner2/Test/temp/orderstest/9991.DON")
+        self.directory_creator.delete_job_directory()
 
     def test_if_directory_is_created_in_job_directory(self):
 
@@ -35,6 +39,10 @@ class TestJobsDirectoryHandler(TestCase):
     def test_if_directory_already_exists_not_try_to_create(self):
 
         expect([self.directory_creator.create_absolute_job_path_directory()]).to.equal([False])
+
+    def test_if_job_directory_is_deleted_when_job_is_finished(self):
+
+        expect([self.directory_creator.delete_job_directory()]).to.equal([True])
 
 
 class TestOrdersDirectoryHandler(TestCase):
