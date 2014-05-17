f26ee4991f019efbb84b1065ead5c400cdbad561
izamarro <ivanzamarro@gmail.com>
2014-05-17 04:32:47 +0200

    mock is not necessary, refactoring test better..

--- Test/test_jobs_worker.py
+++ Test/test_jobs_worker.py
@@ -4,7 +4,7 @@ __author__ = 'izamarro'
 __version__ = '0.0.1a'
 
 import os
-from mock import Mock
+
 from unittest import TestCase
 from expects import expect
 
@@ -17,12 +17,11 @@ class TestJobsDirectoryHandler(TestCase):
 
     def setUp(self):
 
-        self.date_for_job = Mock()
-        self.date_for_job.directory_job = "/home/izamarro/workdev/burner2/Test/temp/jobstest"
-        self.date_for_job.patient_id = "9991"
-        self.date_for_job.absolute_jobpath = self.date_for_job.directory_job + "/%s" % self.date_for_job.patient_id
+        self.directory_job = "/home/izamarro/workdev/burner2/Test/temp/jobstest"
+        self.patient_id = "9991"
+        self.absolute_jobpath = self.directory_job + "/%s" % self.patient_id
 
-        self.directory_creator = JobsDirectoryHandler(self.date_for_job.directory_job, self.date_for_job.patient_id, os)
+        self.directory_creator = JobsDirectoryHandler(self.directory_job, self.patient_id, os)
         self.directory_creator.create_skel_job_directory()
 
     def tearDown(self):
@@ -45,29 +44,28 @@ class TestJobsDirectoryHandler(TestCase):
     def test_if_dictionary_contain_job_directory_skel(self):
 
         expect(self.directory_creator.get_job_skel()).to.have.keys(
-            {"absolute_job_path": self.date_for_job.absolute_jobpath,
-             "data_job_path": (self.date_for_job.absolute_jobpath + "/DATA")})
+            {"absolute_job_path": self.absolute_jobpath,
+             "data_job_path": (self.absolute_jobpath + "/DATA")})
 
 
 class TestOrdersDirectoryHandler(TestCase):
 
     def setUp(self):
 
-        self.file_needs = Mock()
-        self.file_needs.directory = "/home/izamarro/workdev/burner2/Test/temp/orderstest"
-        self.file_needs.name = "9991"
-        self.file_needs.extension = ".DON"
-        self.file_needs.order_path = self.file_needs.directory + "/%s" \
-                                                                 % self.file_needs.name + self.file_needs.extension
-        self.orders_handling = OrdersDirectoryHandler(self.file_needs.directory,
-                                                      self.file_needs.name, self.file_needs.extension, os)
+        self.directory = "/home/izamarro/workdev/burner2/Test/temp/orderstest"
+        self.name = "9991"
+        self.extension = ".DON"
+        self.order_path = self.directory + "/%s" \
+                                                                 % self.name + self.extension
+        self.orders_handling = OrdersDirectoryHandler(self.directory,
+                                                      self.name, self.extension, os)
 
-        with open(self.file_needs.order_path, "w") as tempfile:
+        with open(self.order_path, "w") as tempfile:
             tempfile.close()
 
     def tearDown(self):
 
-        os.remove(os.path.join(self.file_needs.directory, self.file_needs.name + self.file_needs.extension))
+        os.remove(os.path.join(self.directory, self.name + self.extension))
 
     def test_when_i_check_for_extension_and_exist_return_true(self):
 
@@ -78,15 +76,14 @@ class TestJDFFilesHandler(TestCase):
 
     def setUp(self):
 
-        self.jdf_needs = Mock()
-        self.jdf_needs.job_directory = "/home/izamarro/workdev/burner2/Test/temp/jobstest"
-        self.jdf_needs.file_name = "9991.JDF"
-        self.jdf_file_path = self.jdf_needs.job_directory + "/%s" % self.jdf_needs.file_name.split(".")[0] + "/%s" % self.jdf_needs.file_name
+        self.job_directory = "/home/izamarro/workdev/burner2/Test/temp/jobstest"
+        self.file_name = "9991.JDF"
+        self.jdf_file_path = self.job_directory + "/%s" % self.file_name.split(".")[0] + "/%s" % self.file_name
 
-        self.jdf_handler = JDFFilesHandler(self.jdf_needs.job_directory, self.jdf_needs.file_name.split(".")[0], os)
+        self.jdf_handler = JDFFilesHandler(self.job_directory, self.file_name.split(".")[0], os)
 
-        self.directory_handler = JobsDirectoryHandler(self.jdf_needs.job_directory,
-                                                      self.jdf_needs.file_name.split(".")[0], os)
+        self.directory_handler = JobsDirectoryHandler(self.job_directory,
+                                                      self.file_name.split(".")[0], os)
 
         self.directory_handler.create_skel_job_directory()
 
